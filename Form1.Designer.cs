e3ca0b26f530a5c858f1d8e29c5a5867747338d0
SangDepTrai-CTo <sangn6767@gmail.com>
2022-05-31 11:22:48 -0400

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,226 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.MD5text = new System.Windows.Forms.TextBox();
+            this.SHA1text = new System.Windows.Forms.TextBox();
+            this.SHA256text = new System.Windows.Forms.TextBox();
+            this.SHA384text = new System.Windows.Forms.TextBox();
+            this.SHA512text = new System.Windows.Forms.TextBox();
+            this.InPuttext = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SHA3512text = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.ListBoxtire = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // MD5text
+            // 
+            this.MD5text.Location = new System.Drawing.Point(201, 112);
+            this.MD5text.Name = "MD5text";
+            this.MD5text.Size = new System.Drawing.Size(561, 20);
+            this.MD5text.TabIndex = 0;
+            // 
+            // SHA1text
+            // 
+            this.SHA1text.Location = new System.Drawing.Point(201, 159);
+            this.SHA1text.Name = "SHA1text";
+            this.SHA1text.Size = new System.Drawing.Size(561, 20);
+            this.SHA1text.TabIndex = 0;
+            // 
+            // SHA256text
+            // 
+            this.SHA256text.Location = new System.Drawing.Point(201, 207);
+            this.SHA256text.Name = "SHA256text";
+            this.SHA256text.Size = new System.Drawing.Size(561, 20);
+            this.SHA256text.TabIndex = 0;
+            // 
+            // SHA384text
+            // 
+            this.SHA384text.Location = new System.Drawing.Point(201, 263);
+            this.SHA384text.Name = "SHA384text";
+            this.SHA384text.Size = new System.Drawing.Size(561, 20);
+            this.SHA384text.TabIndex = 0;
+            // 
+            // SHA512text
+            // 
+            this.SHA512text.Location = new System.Drawing.Point(201, 314);
+            this.SHA512text.Name = "SHA512text";
+            this.SHA512text.Size = new System.Drawing.Size(561, 20);
+            this.SHA512text.TabIndex = 0;
+            // 
+            // InPuttext
+            // 
+            this.InPuttext.Location = new System.Drawing.Point(326, 57);
+            this.InPuttext.Name = "InPuttext";
+            this.InPuttext.Size = new System.Drawing.Size(317, 20);
+            this.InPuttext.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(666, 51);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(96, 31);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(89, 112);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(30, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "MD5";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(89, 166);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "SHA1";
+            this.label2.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(89, 214);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(50, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "SHA-256";
+            this.label3.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(89, 270);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(50, 13);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "SHA-384";
+            this.label4.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(89, 321);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(50, 13);
+            this.label5.TabIndex = 2;
+            this.label5.Text = "SHA-512";
+            this.label5.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // SHA3512text
+            // 
+            this.SHA3512text.Location = new System.Drawing.Point(201, 367);
+            this.SHA3512text.Name = "SHA3512text";
+            this.SHA3512text.Size = new System.Drawing.Size(561, 20);
+            this.SHA3512text.TabIndex = 0;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(89, 370);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(56, 13);
+            this.label6.TabIndex = 2;
+            this.label6.Text = "SHA3-512";
+            this.label6.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // ListBoxtire
+            // 
+            this.ListBoxtire.FormattingEnabled = true;
+            this.ListBoxtire.Items.AddRange(new object[] {
+            "TEXT String",
+            "HEX String",
+            "FILE"});
+            this.ListBoxtire.Location = new System.Drawing.Point(84, 52);
+            this.ListBoxtire.Name = "ListBoxtire";
+            this.ListBoxtire.Size = new System.Drawing.Size(138, 17);
+            this.ListBoxtire.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.ListBoxtire);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.SHA3512text);
+            this.Controls.Add(this.SHA512text);
+            this.Controls.Add(this.SHA384text);
+            this.Controls.Add(this.SHA256text);
+            this.Controls.Add(this.SHA1text);
+            this.Controls.Add(this.InPuttext);
+            this.Controls.Add(this.MD5text);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox MD5text;
+        private System.Windows.Forms.TextBox SHA1text;
+        private System.Windows.Forms.TextBox SHA256text;
+        private System.Windows.Forms.TextBox SHA384text;
+        private System.Windows.Forms.TextBox SHA512text;
+        private System.Windows.Forms.TextBox InPuttext;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox SHA3512text;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ListBox ListBoxtire;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        string md5_hash(byte[] tmpSource)
+        {
+          
+            //Compute hash based on source data
+            byte[] tmpHash = new MD5CryptoServiceProvider().ComputeHash(tmpSource);
+            string result = BitConverter.ToString(tmpHash);
+            result = result.Replace("-", "").ToLowerInvariant();
+            return result;
+        }
+        string sha1_hash(byte[] tmpSource)
+        {
+
+            //Compute hash based on source data
+            byte[] tmpHash = new SHA1CryptoServiceProvider().ComputeHash(tmpSource);
+            string result = BitConverter.ToString(tmpHash);
+            result = result.Replace("-", "").ToLowerInvariant();
+            return result;
+        }
+        string sha2_hash(byte[] tmpSource, int num)
+        {
+          
+            byte[] tmpHash;
+
+            string result = "";
+            switch (num)
+            {
+                case 256:
+                    //Compute hash based on source data
+                    tmpHash = new SHA256CryptoServiceProvider().ComputeHash(tmpSource);
+
+
+                    result = BitConverter.ToString(tmpHash); ;
+                    break;
+                case 384:
+                    //Compute hash based on source data
+                    tmpHash = new SHA384CryptoServiceProvider().ComputeHash(tmpSource);
+                    result = BitConverter.ToString(tmpHash); ;
+                    break;
+                case 512:
+                    //Compute hash based on source data
+                    tmpHash = new SHA512CryptoServiceProvider().ComputeHash(tmpSource);
+                    result = BitConverter.ToString(tmpHash); ;
+                    break;
+                default:
+                    break;
+            }
+            result = result.Replace("-", "").ToLowerInvariant();
+
+            return result;
+        }
+        string sha3_hash(byte[] tmpSource)
+        {
+            string sSourceData;
+            byte[] tmpHash;
+            var hashAlgorithm = new Org.BouncyCastle.Crypto.Digests.Sha3Digest(512);
+
+            // Choose correct encoding based on your usecase
+
+            hashAlgorithm.BlockUpdate(tmpSource, 0, tmpSource.Length);
+
+            byte[] result = new byte[64]; // 512 / 8 = 64
+            hashAlgorithm.DoFinal(result, 0);
+
+            string hashString = BitConverter.ToString(result);
+            string hash3text = hashString.Replace("-", "").ToLowerInvariant();
+            return hash3text;
+        }
+        public static byte[] StringToByteArray(string hex)
+        {
+            return Enumerable.Range(0, hex.Length)
+                             .Where(x => x % 2 == 0)
+                             .Select(x => Convert.ToByte(hex.Substring(x, 2), 16))
+                             .ToArray();
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MD5text.Text = "";
+            SHA1text.Text = "";
+            SHA256text.Text = "";
+            SHA384text.Text = "";
+            SHA512text.Text = "";
+            SHA3512text.Text = "";
+            string tire = ListBoxtire.GetItemText(ListBoxtire.SelectedItem);
+            if (tire == "TEXT String")
+            {
+                byte[] text_utf8_byte = Encoding.UTF8.GetBytes(InPuttext.Text);
+                if (InPuttext.Text != string.Empty)
+                {
+                    MD5text.Text = md5_hash(text_utf8_byte);
+                    SHA1text.Text = sha1_hash(text_utf8_byte);
+                    SHA256text.Text = sha2_hash(text_utf8_byte, 256);
+                    SHA384text.Text = sha2_hash(text_utf8_byte, 384);
+                    SHA512text.Text = sha2_hash(text_utf8_byte, 512);
+                    SHA3512text.Text = sha3_hash(text_utf8_byte);
+                }
+                else
+                {
+                    MessageBox.Show("Lỗi lòi le bạn ơi", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            else if (tire == "HEX String")
+                    {
+                byte[] text_hex_utf8_byte = StringToByteArray(InPuttext.Text);
+                if (InPuttext.Text.ToString() != string.Empty)
+                {
+                    MD5text.Text = md5_hash(text_hex_utf8_byte);
+                    SHA1text.Text = sha1_hash(text_hex_utf8_byte);
+                    SHA256text.Text = sha2_hash(text_hex_utf8_byte, 256);
+                    SHA384text.Text = sha2_hash(text_hex_utf8_byte, 384);
+                    SHA512text.Text = sha2_hash(text_hex_utf8_byte, 512);
+                    SHA3512text.Text = sha3_hash(text_hex_utf8_byte);
+                }
+                else
+                {
+                    MessageBox.Show("Lỗi lòi le bạn ơi", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            else if (tire =="FILE")
+            {
+                OpenFileDialog ofd = new OpenFileDialog();
+                ofd.ShowDialog();
+                InPuttext.Text = ofd.FileName.ToString();
+                byte[] file_text_byte = File.ReadAllBytes(ofd.FileName.ToString());
+                
+                if (InPuttext.Text.ToString() != string.Empty)
+                {
+                    MD5text.Text = md5_hash(file_text_byte);
+                    SHA1text.Text = sha1_hash(file_text_byte);
+                    SHA256text.Text = sha2_hash(file_text_byte, 256);
+                    SHA384text.Text = sha2_hash(file_text_byte, 384);
+                    SHA512text.Text = sha2_hash(file_text_byte, 512);
+                    SHA3512text.Text = sha3_hash(file_text_byte);
+                }
+                else
+                {
+                    MessageBox.Show("Lỗi lòi le bạn ơi", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }    
+
+
+        }
+    }
+}
